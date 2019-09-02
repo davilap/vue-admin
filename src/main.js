6a90b83e7934102b11429adaba4a58316e992361
Donovan avila pardo <davila@appostools.com>
2019-09-02 14:32:55 -0500

    install i18n

--- src/main.js
+++ src/main.js
@@ -6,16 +6,25 @@ import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
-// custom
+// element ui
 import ElementUI from 'element-ui';
 import './assets/style/element-variables.scss'
 import locale from 'element-ui/lib/locale/lang/en'
 
 Vue.use(ElementUI, { locale })
 
+// i18n
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+
+// lang
+import { i18n } from './lang/i18n'
+
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
