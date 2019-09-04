9db361f1e9f57fa1ba81cbd49f7d24ed8ae9fd7f
Donovan avila pardo <davila@appostools.com>
2019-09-04 18:41:13 -0500

    update ability

--- src/config/ability.js
+++ src/config/ability.js
@@ -1,18 +1,16 @@
 import { AbilityBuilder } from '@casl/ability'
 
-var user = JSON.parse(localStorage.getItem('user'))
+var type = JSON.parse(localStorage.getItem('user'))
 
-function subjectName(item) {
-    if (!item || typeof item === 'string' || !user) {
-        return item
+export default AbilityBuilder.define(can => {
+    switch (type) {
+        case 'guest':
+            can('read', 'Post');
+            break;
+        case 'admin':
+            can('read', 'Post');
+            can(['update', 'delete'], 'Post');
+            break;
+        // Add more roles here
     }
-    else if (item.created_by === user.id || user.is_admin === 1) {
-        return 'Demo'
-    }
-}
-
-export default AbilityBuilder.define({ subjectName }, can => {
-    can(['read'], 'all')
-    if (user) can(['create'], 'all')
-    can(['delete'], 'Demo')
 })
